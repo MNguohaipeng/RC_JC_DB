2949c832aa725823c5d889d6c2b1fad18747726c
123 <12345@.qqcom>
2017-05-02 17:02:06 +0800

    2017/5/2

---
+++ JuCheap.Web/Areas/Adm/Controllers/SizeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JuCheap.Web.Areas.Adm.Controllers
+{
+    public class SizeController : Controller
+    {
+        // GET: Adm/Size
+        public ActionResult Import()
+        {
+            return View();
+        }
+    }
+}
